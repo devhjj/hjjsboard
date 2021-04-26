dcb65c32919654e5fd669abbeadd90a14121d0ce
DevHjj <DevHjj@DESKTOP-66J2KBN>
2021-04-26 21:12:09 +0900

    Error page 변경(403, 404만 숫자 띄우고 나머진 공통 에러 페이지로)

--- hjjsboard/src/main/java/com/study/controller/CustomErrorController.java
+++ hjjsboard/src/main/java/com/study/controller/CustomErrorController.java
@@ -20,10 +20,16 @@ public class CustomErrorController implements ErrorController{
 	@RequestMapping(value = "/error", method= {RequestMethod.GET, RequestMethod.POST}) 
 	public String handleError(HttpServletRequest request, Model model) { 
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		int statusCode = 400;
+		int statusCode = 405; // go to common error page
+		
 		if(status != null){ 
 			statusCode = Integer.valueOf(status.toString());  
 		}
+		
+		if( statusCode != 403 && statusCode != 404 ) { // go to common error page
+			statusCode = 405;
+		}
+		
 		HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
 		model.addAttribute("code", statusCode);
 		model.addAttribute("msg", statusCode + " " + httpStatus.getReasonPhrase());
